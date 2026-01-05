37b017b579e7ab74548db3ea9d75fdd264872a5f
Sai Srikar Bhaskaruni <bsaisrikar05@gmail.com>
2026-01-05 15:13:39 +0530

    Delete svm/app.py

--- svm/app.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-#creating a synthetic dataset
-from sklearn.datasets import make_classification
-
-s, y = make_classification(n_samples = 1000, n_features = 2, n_classes = 2, n_clusters_per_class = 2, n_redundant = 0)
